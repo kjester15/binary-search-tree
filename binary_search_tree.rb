1f67831d9454acba060eb667ff4753ad315ca36a
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-10-05 14:59:26 -0700

    Complete working build_tree method

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,12 +1,12 @@
 require 'pry-byebug'
 
 class Node
-  attr_accessor :value, :left_child, :right_child
+  attr_accessor :data, :left, :right
 
-  def initialize
-    @value = nil
-    @left_child = nil
-    @right_child = nil
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
   end
 
   # include comparable module
@@ -19,4 +19,26 @@ class Tree
     @root = nil
     @array = nil
   end
+
+  def build_tree(array, start_point, end_point)
+    if start_point > end_point then return end
+
+    mid_point = (start_point + end_point) / 2
+    @root = Node.new(array[mid_point])
+    @root.left = Tree.new.build_tree(array, start_point, mid_point - 1)
+    @root.right = Tree.new.build_tree(array, mid_point + 1, end_point)
+
+    @root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
+
+new_tree = Tree.new
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_tree.build_tree(array, 0, array.length - 1)
+new_tree.pretty_print
