432ec80f742ece110a92f303ca9722b0509ebc61
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-10-06 14:01:08 -0700

    Fix issue with insert method to allow finding duplicates at leaf ends

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -31,6 +31,50 @@ class Tree
     @root
   end
 
+  def insert(value)
+    current_node = self.root
+    # traverse nodes until reaching leaf
+    loop do
+      if value == current_node.data
+        puts 'Duplicate value found - node not inserted.'
+        return
+      elsif value < current_node.data
+        if current_node.left.nil? && current_node.right.nil? then break end
+
+        current_node = current_node.left
+      elsif value > current_node.data
+        if current_node.left.nil? && current_node.right.nil? then break end
+
+        current_node = current_node.right
+      end
+    end
+    # create node on appropriate side of leaf
+    if value < current_node.data
+      current_node.left = Node.new(value)
+    else
+      current_node.right = Node.new(value)
+    end
+  end
+
+  def delete(value)
+    current_node = self.root
+    # traverse nodes until reaching leaf
+    loop do
+      if value == current_node.data
+        puts 'Found node to delete.'
+        return
+      elsif value < current_node.data
+        if current_node.left.nil? && current_node.right.nil? then break end
+
+        current_node = current_node.left
+      elsif value > current_node.data
+        if current_node.left.nil? && current_node.right.nil? then break end
+
+        current_node = current_node.right
+      end
+    end
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -41,4 +85,7 @@ end
 new_tree = Tree.new
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 new_tree.build_tree(array, 0, array.length - 1)
+# new_tree.pretty_print
+new_tree.insert(5)
 new_tree.pretty_print
+new_tree.delete(7)
